2dbd7684d067b0b75ef8b4e5a92dbd87cfedc270
Woodii <33593572+Woodii1998@users.noreply.github.com>
2023-08-01 17:51:16 +0800

    init

---
+++ scripts/split.ts
@@ -0,0 +1,40 @@
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+import { CustomMarkdownLoader } from '../utils/customPDFLoader';
+import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
+
+/* Name of directory to retrieve your files from 
+   Make sure to add your PDF files inside the 'docs' folder
+*/
+const filePath = 'docs';
+
+export const run = async () => {
+  try {
+    /*load raw docs from the all files in the directory */
+    const directoryLoader = new DirectoryLoader(filePath, {
+      '.pdf': (path) => new PDFLoader(path),
+      '.md': (path) => new CustomMarkdownLoader(path),
+    });
+
+    const rawDocs = await directoryLoader.load();
+
+    /* Split text into chunks */
+    const textSplitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 1000,
+      chunkOverlap: 200,
+    });
+
+    const docs = await textSplitter.splitDocuments(rawDocs);
+
+    console.log('doc:', docs[50].pageContent);
+    console.log('lines:', docs[50].metadata.loc);
+  } catch (error) {
+    console.log('error', error);
+    throw new Error('Failed to ingest your data');
+  }
+};
+
+(async () => {
+  await run();
+  console.log('split');
+})();
